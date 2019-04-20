a8cdee0e06f6bb96f882d72aebf2adb5d0a8f9c3
ReDrawCompany <49760297+ReDrawCompany@users.noreply.github.com>
2019-04-20 10:20:04 -0400

    peep

--- bfparse/main.js
+++ bfparse/main.js
@@ -1,3 +1,14 @@
 import brainfuck from "./brainfuck.js";
 
+function html(test) {
+    let a = document.createElement("div");
+    a.innerHTML = test;
+    document.appendChild(a.firstChild);
+    return a.firstChild;
+}
+
 window.bf = brainfuck;
+
+let a = new brainfuck;
+window.a = a;
+
